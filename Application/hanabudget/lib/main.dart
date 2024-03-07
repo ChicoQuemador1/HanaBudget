887d5c338ba57cbf0b6e36b0189801cafdabdc29
Mark Denina <madenina354@gmail.com>
2024-03-07 13:24:06 -0500

    Creates Home Page with Four Buttons to act as Routes to different Actions

--- Application/hanabudget/lib/main.dart
+++ Application/hanabudget/lib/main.dart
@@ -1,19 +1,115 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      title: 'Flutter Navigation Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+      routes: {
+        '/page1': (context) => Page1(),
+        '/page2': (context) => Page2(),
+        '/page3': (context) => Page3(),
+        '/page4': (context) => Page4(),
+      },
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        children: <Widget>[
+          _buildGridItem(context, 'Page 1', '/page1'),
+          _buildGridItem(context, 'Page 2', '/page2'),
+          _buildGridItem(context, 'Page 3', '/page3'),
+          _buildGridItem(context, 'Page 4', '/page4'),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildGridItem(BuildContext context, String title, String route) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, route);
+      },
+      child: Card(
+        child: Center(
+          child: Text(
+            title,
+            style: TextStyle(fontSize: 20.0),
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class Page1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page 1'),
+      ),
+      body: Center(
+        child: Text('This is Page 1'),
+      ),
+    );
+  }
+}
+
+class Page2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page 2'),
+      ),
+      body: Center(
+        child: Text('This is Page 2'),
+      ),
+    );
+  }
+}
+
+class Page3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page 3'),
+      ),
+      body: Center(
+        child: Text('This is Page 3'),
+      ),
+    );
+  }
+}
+
+class Page4 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page 4'),
+      ),
+      body: Center(
+        child: Text('This is Page 4'),
       ),
     );
   }
