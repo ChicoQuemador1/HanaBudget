530f8dabc9432c89ee08ff825157dd7e4a6db36c
Mark Denina <madenina354@gmail.com>
2024-03-14 19:30:07 -0400

    Adds outline of Login Page - Does not include working process to go to home page or other pages

--- Application/hanabudget/lib/main.dart
+++ Application/hanabudget/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'pages/loginPage.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: LoginPage(),
       routes: {
         '/page1': (context) => ViewExpenses(),
         '/page2': (context) => ViewBudgets(),
