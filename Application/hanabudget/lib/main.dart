f6f56cfc8ad412936101af5316cd88de94f1e5b5
Alexis <delaalexis404@gmail.com>
2024-03-15 19:11:12 -0400

    Update main.dart

--- Application/hanabudget/lib/main.dart
+++ Application/hanabudget/lib/main.dart
@@ -28,9 +28,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-      ),
+      appBar: AppBar(),
       body: GridView.count(
         crossAxisCount: 2,
         children: <Widget>[
